6c0686cc6d482decb5247f27e2b2f45bc9bfc639
川崎 玄勢 <mov.an.double@gmail.com>
2015-08-04 01:22:11 +0900

    画像アップロードできるまで t:2

---
+++ app/controllers/stamps_controller.rb
@@ -0,0 +1,29 @@
+class StampsController < ApplicationController
+  before_filter :set_new_image
+
+  def index
+    @stamps = Stamp.all
+  end
+
+  def show
+    @image = Stamp.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    @new_image.upload_from_params!(stamp_params)
+    redirect_to action: :show, id: @new_image.id
+  end
+
+  private
+
+  def set_new_image
+    @new_image = Stamp.new
+  end
+
+  def stamp_params
+    params.require(:stamp).permit(:image)
+  end
+end
