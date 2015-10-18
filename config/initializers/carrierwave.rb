00053454161758cf152c329f41b7de5b720e34fc
川崎 玄勢 <mov.an.double@gmail.com>
2015-10-18 19:17:25 +0900

    fix carrierwave  t:2

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,14 +1,12 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: '<%= Setting.aws.aws_access_key_id %>',
-    aws_secret_access_key: '<%= Setting.aws.aws_secret_access_key %>',
-    region: 'ap-northeaset-1',
-    path_style: true
+    aws_access_key_id: Settings.aws.aws_access_key_id,
+    aws_secret_access_key: Settings.aws.aws_secret_access_key,
+    endpoint: "https://s3.amazonaws.com",
+    region: 'ap-northeast-1'
   }
 
   config.fog_directory = 'kiramemo'
-  #config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/kiramemo'
   config.fog_public = false
-  config.fog_attributes = {'Cache-Control' => 'public, max-age=86400'}
 end
