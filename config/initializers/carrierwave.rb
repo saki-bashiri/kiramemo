33c6e4c6820d99b8c0f5b81a7dece0be781479bd
川崎 玄勢 <mov.an.double@gmail.com>
2015-11-06 23:43:32 +0900

    環境変数に変更 t:3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: Settings.aws.aws_access_key_id,
-    aws_secret_access_key: Settings.aws.aws_secret_access_key,
+    aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+    aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
     endpoint: "https://s3.amazonaws.com",
     region: 'ap-northeast-1'
   }
